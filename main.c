6ecfeeeaf81a35193fce2a692276b10d9ba3b14d
matheussannomiya <mamellinho@gmail.com>
2026-05-13 12:55:25 -0300

    feat: replace linear probing with separate chaining for hash collision resolution

--- main.c
+++ main.c
@@ -5,27 +5,36 @@
 
 #define TAM 31
 
-typedef struct{
+typedef struct data{
     char user[20];
     char password[20];
     int id;
+    struct data *next;
 }Data;
 
-int load_txt(Data base[]);
-void login(Data base[], int totalusers);
-int registeer(Data base[], int totalusers);
+typedef struct{
+    Data *head;
+}Bucket;
+
+
+int load_txt(Bucket base[]);
+void login(Bucket base[], int totalusers);
+int registeer(Bucket base[], int totalusers);
 void password_login(char correct_password[], Data base[], char user[]);
 void syncToFile(Data new);
 int hash(int key);
 int hashString(char s[]);
-void clearTable(Data member[]);
-int idValidation(Data member[], int iduser);
-void insertTable(Data member[], Data u);
-Data *searchTable(Data member[], char user[]);
+void clearTable(Bucket member[]);
+int idValidation(Bucket member[], int iduser);
+void insertTable(Bucket member[], Data u);
+Data *searchTable(Bucket member[], char user[]);
+void clearList(Bucket *l);
+void insertList(Bucket *l, Data user);
+Data *searchList(Bucket *l, char user[]);
 
 int main(){
     srand(time(NULL));
-    Data member[TAM];
+    Bucket member[TAM];
     clearTable(member);
     
     int option = 0;
@@ -48,39 +57,40 @@ int main(){
     return 0;
 }
 
-void login(Data base[], int totalusers){
+void login(Bucket base[], int totalusers){
     char username_typed[20];
     char password[20];
     char username[20];
-    int flag = 0;
+    Data *found;
 
     printf("\nInsert your username: ");
     while(getchar() != '\n');
     fgets(username_typed, 20, stdin);
     username_typed[strcspn(username_typed, "\n")] = '\0';
-    int index = hashString(username_typed);
+    found = searchTable(base, username_typed);
 
-    if(strcmp(username_typed, base[index].user) == 0){
-        flag = 0;
-        password_login(base[index].password, base, base[index].user);
+    if(found != NULL){
+        password_login(found->password, found, found->user);
     }else{
         printf("User not found\n");
     }
 
 }
 
-int registeer(Data base[], int totalusers){
+int registeer(Bucket base[], int totalusers){
     Data new;
     printf("\n********REGISTER********\n");
+    Data *result;
 
     do{
     printf("\nUser: ");
     while(getchar() != '\n');
     fgets(new.user, 20, stdin);
     new.user[strcspn(new.user, "\n")] = '\0';
-    if(searchTable(base, new.user) != NULL)
+    result = searchTable(base, new.user);
+    if(result != NULL)
         printf("Invalid username (already being used).\n");
-    }while(searchTable(base, new.user) != NULL);
+    }while(result != NULL);
     
     printf("\nPassword: ");
     fgets(new.password, 20, stdin);
@@ -134,7 +144,7 @@ void password_login(char correct_password[], Data base[], char user[]){
     fclose(log);
 }
 
-int load_txt(Data base[]){
+int load_txt(Bucket base[]){
     Data aux;
     FILE *file = fopen("datasystem.txt", "a+");
 
@@ -176,34 +186,56 @@ int hashString(char s[]){
     return hash % TAM;
 }
 
-void clearTable(Data member[]){
-    for(int i = 0; i < TAM; i++){
-        strcpy(member[i].user, "");
-    }
+void clearTable(Bucket member[]){
+    for(int i = 0; i < TAM; i++)
+        clearList(&member[i]);
 }
 
-void insertTable(Data member[], Data u){
+void insertTable(Bucket member[], Data u){
     int id = hashString(u.user);
-    /*while(strlen(member[id].user) > 0)
-        id = hash(id + 1);*/
-    member[id] = u;
+    insertList(&member[id], u);
 }
 
-int idValidation(Data member[], int iduser){
+int idValidation(Bucket member[], int iduser){
     for(int x = 0; x < TAM; x++){
-        if(member[x].id == iduser)
+        Data *aux = member[x].head;
+        while(aux != NULL && aux->id != iduser)
+            aux = aux->next;
+        if(aux != NULL)
             return 1;
     }
     return 0;
 }
 
-Data *searchTable(Data member[], char user[]){
+Data *searchTable(Bucket member[], char user[]){
     int index = hashString(user);
-
-    if(strcmp(member[index].user, user) == 0)
-        return &member[index];
-    else{
-        return NULL;
-    }
-    
+    searchList(&member[index], user);
 }
+
+void clearList(Bucket *l){
+    l->head = NULL;
+}
+
+void insertList(Bucket *l, Data user){
+    Data *new = malloc(sizeof(Data));
+    if(new != NULL){
+        strcpy(new->user, user.user);
+        strcpy(new->password, user.password);
+        new->id = user.id;
+        new->next = l->head;
+        l->head = new;
+    }else{
+        printf("Malloc failed.\n");
+    }
+}
+
+Data *searchList(Bucket *l, char user[]){
+    Data *aux = l->head;
+    while(aux != NULL && strcmp(aux->user, user) != 0)
+        aux = aux->next;
+    if(aux != NULL){
+        return aux;
+    }
+    return NULL;
+}
+
